9e31427600c694e06220047cc04118278aa78332
Menat Pinesh I <n01205889@nl-00205-017.mediastudies.ca>
2018-03-28 21:31:59 -0400

    With Product Entity again

--- CoreDataTableView/ViewController.swift
+++ CoreDataTableView/ViewController.swift
@@ -19,16 +19,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let name = productNameTV.text ?? "New Product"
         let price = Double(productPriceTV.text ?? "0.0")!
         let id = UUID.init()
-        products.append(ProductType(pId: id,pName: name,pPrice: price))
+        //products.append(ProductType(pId: id,pName: name,pPrice: price))
+        
+        
+        //Save the new item in the SQLite DB
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        let newProduct = Product(context: context)
+        newProduct.id = id
+        newProduct.name = name
+        newProduct.price = price
+        delegate.saveContext()
+        loadData()
         tableView.reloadData()
     }
     
-    var products : Array<ProductType> = []
+    func loadData() {
+    //Load Data
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let context = delegate.persistentContainer.viewContext
+    do {
+    products = try context.fetch(Product.fetchRequest())
+    } catch {
+    
+    }
+    }
+    
+    var products : Array<Product> = []
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Load data
+        loadData()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -41,10 +66,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1")
-        cell?.textLabel?.text = products[indexPath.row].getName()
-        cell?.detailTextLabel?.text = String(products[indexPath.row].getPrice())
+        cell?.textLabel?.text = products[indexPath.row].name
+        cell?.detailTextLabel?.text = String(products[indexPath.row].price)
         return cell!
     }
 
 }
-
